1e0674d94df60cec98b1ca398119dd5da0d62926
Muhammad Muazzain <90068510+MuhammadMuazzain@users.noreply.github.com>
2025-08-20 19:36:17 +0100

    Logic Update

--- conversion/run_tf_model.py
+++ conversion/run_tf_model.py
@@ -7,5 +7,8 @@ model.trainable = False
 
 input_tensor = tf.random.uniform([1, 3, 640, 640])
 
+# out = model(**{'input': input_tensor})
+# print(out)
+
 out = model(**{'input': input_tensor})
 print(out)
--- conversion/run_tflite_model.py
+++ conversion/run_tflite_model.py
@@ -19,5 +19,10 @@ interpreter.invoke()
 
 # get_tensor() returns a copy of the tensor data
 # use tensor() in order to get a pointer to the tensor
+# output_data = interpreter.get_tensor(output_details[0]['index'])
+# print(output_data)
+
+
+
 output_data = interpreter.get_tensor(output_details[0]['index'])
 print(output_data)
