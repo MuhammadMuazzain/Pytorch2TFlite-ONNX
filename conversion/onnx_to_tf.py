3434ace155f9e08decd4fc4cce48553bb34ce205
Muhammad Muazzain <90068510+MuhammadMuazzain@users.noreply.github.com>
2025-08-20 19:38:35 +0100

    Logic Update

--- conversion/onnx_to_tf.py
+++ conversion/onnx_to_tf.py
@@ -2,6 +2,8 @@ from onnx_tf.backend import prepare
 import onnx
 
 onnx_model_path = 'model.onnx'
+# tf_model_path = 'model_tf'
+
 tf_model_path = 'model_tf'
 
 # onnx_model = onnx.load(onnx_model_path)
